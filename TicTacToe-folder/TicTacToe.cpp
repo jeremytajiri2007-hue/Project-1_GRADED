8cb04b4fc7e4386c720022acebe26f442976c0e1
jeremytajiri2007-hue <jeremytajiri2007@gmail.com>
2025-11-16 17:41:58 -0800

    Update TicTacToe.cpp

--- TicTacToe-folder/TicTacToe.cpp
+++ TicTacToe-folder/TicTacToe.cpp
@@ -3,36 +3,32 @@
 // Name: Jeremy Tajiri
 // Course: CISC 192 - C++ Programming
 // Date: 11/16/2025
-// Assignment: Tic-Tac-Toe Game (Class-Based)
+// Assignment: Tic-Tac-Toe Game
 
 #include <iostream>
 using namespace std;
 
 class TicTacToe {
 private:
-    char board[3][3];   // 3x3 game board
-    char currentPlayer; // 'X' or 'O'
-    int moveCount;      // count of moves made
+    char board[3][3];
+    char currentPlayer;
+    int moveCount;
 
-    void switchPlayer() { // switch player turn
-        if(currentPlayer == 'X')
-            currentPlayer = 'O';
-        else
-            currentPlayer = 'X';
+    void switchPlayer() {
+        if(currentPlayer == 'X') currentPlayer = 'O';
+        else currentPlayer = 'X';
     }
 
 public:
-    TicTacToe() { // constructor
+    TicTacToe() {
         currentPlayer = 'X';
         moveCount = 0;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                board[i][j] = ' '; // empty board
-            }
-        }
+        for(int i=0;i<3;i++)
+            for(int j=0;j<3;j++)
+                board[i][j] = ' ';
     }
 
-    void displayBoard() { // show the board
+    void displayBoard() {
         cout << "\n";
         for(int i=0;i<3;i++){
             cout << " ";
@@ -46,72 +42,64 @@ public:
         cout << "\n";
     }
 
-    bool isValidMove(int row, int col){ // check move
-        if(row>=0 && row<3 && col>=0 && col<3){
-            if(board[row][col]==' ') return true;
-        }
-        return false;
+    bool isValidMove(int row, int col){
+        if(row<0 || row>2 || col<0 || col>2) return false;
+        if(board[row][col] != ' ') return false;
+        return true;
     }
 
-    void makeMove(int row,int col){ // put move on board
+    void makeMove(int row,int col){
         if(isValidMove(row,col)){
             board[row][col] = currentPlayer;
             moveCount++;
         } else {
-            cout << "Oops, that move is not allowed!\n";
+            cout << "Hey that spot's taken or invalid!\n";
         }
     }
 
-    bool checkWinner(){ // check for win
-        // check rows and cols
+    bool checkWinner(){
         for(int i=0;i<3;i++){
-            if(board[i][0]==currentPlayer && board[i][1]==currentPlayer && board[i][2]==currentPlayer)
-                return true;
-            if(board[0][i]==currentPlayer && board[1][i]==currentPlayer && board[2][i]==currentPlayer)
-                return true;
+            if(board[i][0]==currentPlayer && board[i][1]==currentPlayer && board[i][2]==currentPlayer) return true;
+            if(board[0][i]==currentPlayer && board[1][i]==currentPlayer && board[2][i]==currentPlayer) return true;
         }
-        // check diagonals
-        if(board[0][0]==currentPlayer && board[1][1]==currentPlayer && board[2][2]==currentPlayer)
-            return true;
-        if(board[0][2]==currentPlayer && board[1][1]==currentPlayer && board[2][0]==currentPlayer)
-            return true;
-
+        if(board[0][0]==currentPlayer && board[1][1]==currentPlayer && board[2][2]==currentPlayer) return true;
+        if(board[0][2]==currentPlayer && board[1][1]==currentPlayer && board[2][0]==currentPlayer) return true;
         return false;
     }
 
-    bool isDraw(){ // check draw
+    bool isDraw(){
         if(moveCount>=9) return true;
-        return false;
+        else return false;
     }
 
-    void playGame(){ // main game loop
+    void playGame(){
         int row, col;
         bool gameOver = false;
 
-        cout << "Welcome to TicTacToe!\n";
+        cout << "Let's play Tic-Tac-Toe!\n";
         displayBoard();
 
         while(!gameOver){
-            cout << "Player " << currentPlayer << " enter row and col (1-3): ";
+            cout << "Player " << currentPlayer << ", pick row and col (1-3): ";
             cin >> row >> col;
-            row--; col--; // convert to 0-based index
+            row--; col--;
 
             if(isValidMove(row,col)){
                 makeMove(row,col);
                 displayBoard();
 
                 if(checkWinner()){
-                    cout << "Player " << currentPlayer << " won!!!\n";
+                    cout << "Player " << currentPlayer << " wins!!!\n";
                     gameOver = true;
                 } else if(isDraw()){
-                    cout << "Its a draw!\n";
+                    cout << "It's a draw I guess...\n";
                     gameOver = true;
                 } else {
                     switchPlayer();
                 }
 
             } else {
-                cout << "Not a valid move, try again\n";
+                cout << "Nope, try another spot.\n";
             }
         }
     }
